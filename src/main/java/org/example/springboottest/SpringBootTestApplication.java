f8cb820d7cf58ccf785ad4cf01da7e052ea376b8
Alin Bordeianu <martynnn33@gmail.com>
2025-10-23 11:56:03 +0200

    feat: created project

---
+++ src/main/java/org/example/springboottest/SpringBootTestApplication.java
@@ -0,0 +1,23 @@
+package org.example.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootTestApplication {
+
+    void main(String[] args) {
+        SpringApplication.run(SpringBootTestApplication.class, args);
+    }
+
+    @GetMapping("/hello") // maps GET requests to /hello to this sayHello() method.
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+        // @RequestParam expects a value called myName in the request it receives and maps it to the name variable
+        return String.format("Hello, %s!", name);
+    }
+
+}
